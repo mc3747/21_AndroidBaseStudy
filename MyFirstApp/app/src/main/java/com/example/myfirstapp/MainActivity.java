3ee6dcb7f5ce1a39113135c9d6af977e52ebce3e
mc3747 <mc3747@126.com>
2019-12-19 15:12:46 +0800

    跳转

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(android.view.View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        TextView editText = (TextView) findViewById(R.id.textView1);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
